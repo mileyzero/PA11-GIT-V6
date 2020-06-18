312e242bbd2e69a4542a39ed6cca9575fa610c3d
mileyzero <64124335+mileyzero@users.noreply.github.com>
2020-06-18 19:31:04 +0800

    Updated

---
+++ 28_PA11_Jerome/Assets/Scripts/AddScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour
+{
+    public float addScore;
+    public Text Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            addScore += 10;
+            Score.text = ("Score: " + addScore);
+        }
+    }
+}
---
+++ 28_PA11_Jerome/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject;
+    public GameObject SpawnObject1;
+    public GameObject SpawnObject2;
+    float PositionY;
+    float Rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        Rand = Random.Range(1, 4);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+
+        if(Rand == 1)
+        {
+            Instantiate(SpawnObject, transform.position, transform.rotation);
+        }
+
+        if(Rand == 2)
+        {
+            Instantiate(SpawnObject1, transform.position, transform.rotation);
+        }
+
+        if(Rand == 3)
+        {
+            Instantiate(SpawnObject2, transform.position, transform.rotation);
+        }
+    }
+}
